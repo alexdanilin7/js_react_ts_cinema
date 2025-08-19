65850afc7b9cef13f473f2d004c38a2cc64add5f
andanilin <andanilin7@gmail.com>
2025-08-19 23:12:40 +0200

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/js_react_ts_cinema/",
   plugins: [react()],
+
 })
